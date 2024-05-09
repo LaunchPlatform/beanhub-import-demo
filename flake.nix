0885c9c69c7acc1a3a8f480114e9203deb52e206
Fang-Pen Lin <hello@fangpenlin.com>
2024-05-09 13:56:08 -0700

    Add nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    flake-utils = {
+      url = "github:numtide/flake-utils/v1.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ attrs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        formatter = pkgs.alejandra;
+        devShells.default = pkgs.mkShell {
+          env = {
+            LC_ALL = "C.UTF-8";
+          };
+          packages = with pkgs; [
+            python312
+          ];
+        };
+      }
+    );
+}
